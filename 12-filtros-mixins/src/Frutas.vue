c6df94a824b3430df4b86564758489f8171d36c8
NicolasES <nicolas.escouto@gmail.com>
2019-04-27 11:27:03 -0300

    diretivas, filtros, mixins

---
+++ 12-filtros-mixins/src/Frutas.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="fruta in frutas" :key="fruta"> {{ fruta }} </li>
+        </ul>
+        <input type="text" v-model="fruta" @keydown.enter="add">
+    </div>
+</template>
+
+<script>
+import futasMixin from './frutasMixin'
+export default {
+   mixins: [futasMixin]
+}
+</script>
+
+<style>
+
+</style>
