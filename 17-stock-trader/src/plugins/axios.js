b698919fa3a9b76bbc1af76a18382ee2357b8857
NicolasES <nicolas.escouto@gmail.com>
2019-06-16 14:18:22 -0300

    making it better

---
+++ 17-stock-trader/src/plugins/axios.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import axios from 'axios'
+
+Vue.use({
+    install(Vue) {
+        Vue.prototype.$http = axios.create({
+            baseURL: 'https://stock-trader-836e2.firebaseio.com/'
+        })
+    }
+})
